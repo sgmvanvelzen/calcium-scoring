dd15fd0b9cb2c096ec14c05c3a1eb0975e524a71
Sanne van Velzen <57665095+sgmvanvelzen@users.noreply.github.com>
2019-11-16 21:50:31 +0100

    Update TrainNetwork1.py
    fixed visdom parameter

--- src/TrainNetwork1.py
+++ src/TrainNetwork1.py
@@ -36,7 +36,7 @@ config = {
 parser = ArgumentParser()
 parser.add_argument('--inputdir', default='/home/user/input')
 parser.add_argument('--scratchdir', default='/home/user/scratch')
-parser.add_argument('--visdom')
+parser.add_argument('--visdom', default=None)
 parser.add_argument('--kernels', default='all')  # soft/sharp
 
 # ----------------------------------------------------------------------------------------------------------------------
