4e47e93a168f2d296267641c63e5131e0b870747
Sanne van Velzen <57665095+sgmvanvelzen@users.noreply.github.com>
2022-12-05 10:29:03 +0100

    Delete src directory
    Move to QurAI

--- src/calciumscoring/utils.py
+++
@@ -1,8 +0,0 @@
-# Copyright: (c) 2019, University Medical Center Utrecht 
-# GNU General Public License v3.0+ (see LICNSE or https://www.gnu.org/licenses/gpl-3.0.txt)
-
-import numpy as np
-
-
-def random_item(items):
-    return items[np.random.randint(len(items))]
